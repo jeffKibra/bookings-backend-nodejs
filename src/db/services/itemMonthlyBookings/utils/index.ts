b524e28e6d5f092c399298e86c7c1015660e45dc
jeffKibra <59401150+jeffKibra@users.noreply.github.com>
2024-01-19 14:08:25 +0300

    deleted item monthly bookings db/services folder.

--- src/db/services/itemMonthlyBookings/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as getBookingDatesMapping } from './getBookingDatesMapping';
-export { default as generateIncomingDates } from './generateIncomingDates';
-export { default as updateItemBookings } from './updateItemBookings';
-export { default as updateItemMonthlyBookings } from './updateItemMonthlyBookings';
